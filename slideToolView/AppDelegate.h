16c7ba23640c84cad359d7595566e6ea240267eb
张辉男 <475816908@qq.com>
2017-02-20 15:25:32 +0800

    Initial Commit

---
+++ slideToolView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  slideToolView
+//
+//  Created by 张辉男 on 17/2/20.
+//  Copyright © 2017年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
